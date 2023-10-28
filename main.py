257e21d16fc29146264e4a07d4eb3b840e1acc63
denyanenko <108097508+denyanenko@users.noreply.github.com>
2023-10-28 10:51:54 +0300

    second

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import time
+import random
+
+# region LDFS Algorithm Implementation
 class NQueens:
-    def __init__(self):
+    def __init__(self, start):
         self.size = 8
-        positions = []
-        # Початковий стан дошки, всі ферзі розташовані у перший стовпчик.
-        positions = [4, 0, 6, 4, 3, 7, 5, 5]
-        print("Початкове розташування: ", positions)
+        positions=start[:]
         try:
             start_time = time.time()
             max_execution_time = 60  # Обмеження на час виконання програми (60 секунд).
@@ -37,7 +37,115 @@ class NQueens:
                     positions[i] + i == column + ocuppied_rows:
                 return False
         return True
+# endregion
+
+# region A* Algorithm Implementation
+class Node():
+    def __init__(self, parent=None, state=None):
+        self.parent = parent
+        self.state = state
+        self.collisions = [0, 0, 0, 0, 0, 0, 0, 0]
+        self.h = 0
+
+    def __eq__(self, other):
+        return self.state == other.state
 
 
-# Створюємо об'єкт класу NQueens, щоб розпочати пошук розв'язку.
-NQueens()
+def astar(start):
+    start_node = Node(None, start)
+    start_node.h = heuristic(start, start_node.collisions)
+
+    open_list = []
+    closed_list = []
+
+    open_list.append(start_node)
+
+    start_time = time.time()
+    max_execution_time = 60  # Обмеження на час виконання програми (60 секунд).
+
+    while len(open_list) > 0:
+        current_node = open_list[0]
+        current_index = 0
+        # Перевірка часу виконання
+        if (time.time() - start_time) > max_execution_time:
+            print("Час виконання перевищив обмеження.")
+            return None
+        for index, item in enumerate(open_list):
+            if item.h < current_node.h:
+                current_node = item
+                current_index = index
+            elif item.h > current_node.h + 2:
+                open_list.remove(item)
+
+        open_list.pop(current_index)
+        closed_list.append(current_node)
+
+        if current_node.h == 0:
+            print("Результат розташування: ", current_node.state)
+            print((time.time() - start_time), "seconds")
+            return current_node.state
+
+        children = []
+        for collisioned in range(0, 8):
+            if current_node.collisions[collisioned]:
+                for new_state in [1, 2, 3, 4, 5, 6, 7]:
+                    new_node = Node(current_node, current_node.state.copy())
+
+                    new_node.state[collisioned] = (new_node.state[collisioned] + new_state) % 8
+
+                    children.append(new_node)
+        for child in children:
+
+            skip = False
+            for closed_child in closed_list:
+                if child == closed_child:
+                    skip = True
+            if skip: continue
+
+            child.h = heuristic(child.state, child.collisions)
+
+            for open_node in open_list:
+                if child == open_node and child.h >= open_node.h:
+                    skip = True
+            if skip: continue
+
+            open_list.append(child)
+
+
+def heuristic(state, collisions):
+    h = 0
+    rowLock = -1
+    upperDiagLock = -1
+    lowerDiagLock = -1
+    for i in range(0, 8):
+        for j in range(i + 1, 8):
+            if state[i] == state[j] and rowLock != i:
+                rowLock = i
+                collisions[i] = collisions[j] = 1
+                h += 1
+            elif state[i] - state[j] == i - j and upperDiagLock != i:
+                upperDiagLock = i
+                collisions[i] = collisions[j] = 1
+                h += 1
+            elif state[i] - state[j] == - (i - j) and lowerDiagLock != i:
+                lowerDiagLock = i
+                collisions[i] = collisions[j] = 1
+                h += 1
+    return h
+# endregion
+
+def main():
+    start = []
+    for i in range(0, 8):
+        start.append(random.randint(0, 7))
+    print("Початкове розташування: ", start,"\n")
+    print("LDFS")
+    NQueens(start)
+    print()
+    print("A*")
+    astar(start)
+
+
+if __name__ == '__main__':
+    main()
+
